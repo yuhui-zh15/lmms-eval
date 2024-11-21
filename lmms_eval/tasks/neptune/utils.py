2d7ee1f125c8e443936bd73dc65e245b0efdb2e8
Bo Li <drluodian@gmail.com>
2024-11-21 03:29:34 +0000

    🔧 chore(neptune): update YAML configuration and improve path handling
    - change dataset_name from 'neptune_full' to 'full'
    - add 'task' field in YAML configuration for clarity
    
    ♻️ refactor(neptune utils): enhance video path handling logic
    
    - add fallback logic to handle different video file extensions
    - improve cache directory determination using environment variable

--- lmms_eval/tasks/neptune/utils.py
+++ lmms_eval/tasks/neptune/utils.py
@@ -5,9 +5,32 @@ from loguru import logger
 
 from lmms_eval.tasks._task_utils.file_utils import generate_submission_file
 
+hf_home = os.getenv("HF_HOME", "~/.cache/huggingface/")
+# cache_dir = os.path.join(hf_home, cache_dir)
+# base_cache_dir = config["dataset_kwargs"]["cache_dir"]
+base_cache_dir = os.path.expanduser(hf_home)
+with open(Path(__file__).parent / "neptune_full.yaml", "r") as f:
+    raw_data = f.readlines()
+    safe_data = []
+    for i, line in enumerate(raw_data):
+        # remove function definition since yaml load cannot handle it
+        if "!function" not in line:
+            safe_data.append(line)
+cache_name = yaml.safe_load("".join(safe_data))["dataset_kwargs"]["cache_dir"]
 
 def neptune_full_doc_to_visual(doc):
-    return [doc["video_path"]]
+    cache_dir = os.path.join(base_cache_dir, cache_name)
+    video_path = doc["video_path"] + ".mp4"
+    video_path = os.path.join(cache_dir, "data", video_path)
+    if os.path.exists(video_path):
+        video_path = video_path
+    elif os.path.exists(video_path.replace("mp4", "MP4")):
+        video_path = video_path.replace("mp4", "MP4")
+    elif os.path.exists(video_path.replace("mp4", "mkv")):
+        video_path = video_path.replace("mp4", "mkv")
+    else:
+        sys.exit(f"video path:{video_path} does not exist, please check")
+    return [video_path]
 
 
 def neptune_full_doc_to_text(doc, lmms_eval_specific_kwargs):
